08d5926817e938361793e0f29097e76da95ca2f7
Guilherme Revolta <guilhermerevolta@gmail.com>
2021-02-18 08:55:41 -0300

    feat: eslint and prettier config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
